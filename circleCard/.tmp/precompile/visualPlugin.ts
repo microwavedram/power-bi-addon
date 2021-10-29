65462861e048c3cb9b019010b10bf8d4b67d67da
microwavedram <89029844+microwavedram@users.noreply.github.com>
2021-10-29 20:39:34 +0100

    first

---
+++ circleCard/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,32 @@
+import { Visual } from "../../src/visual";
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions;
+import DialogConstructorOptions = powerbiVisualsApi.extensibility.visual.DialogConstructorOptions;
+var powerbiKey: any = "powerbi";
+var powerbi: any = window[powerbiKey];
+var circleCard6B524826D5D448269A0C4E1DDA156637_DEBUG: IVisualPlugin = {
+    name: 'circleCard6B524826D5D448269A0C4E1DDA156637_DEBUG',
+    displayName: 'CircleCard',
+    class: 'Visual',
+    apiVersion: '3.8.0',
+    create: (options: VisualConstructorOptions) => {
+        if (Visual) {
+            return new Visual(options);
+        }
+        throw 'Visual instance not found';
+    },
+    createModalDialog: (dialogId: string, options: DialogConstructorOptions, initialState: object) => {
+        const dialogRegistry = globalThis.dialogRegistry;
+        if (dialogId in dialogRegistry) {
+            new dialogRegistry[dialogId](options, initialState);
+        }
+    },
+    custom: true
+};
+if (typeof powerbi !== "undefined") {
+    powerbi.visuals = powerbi.visuals || {};
+    powerbi.visuals.plugins = powerbi.visuals.plugins || {};
+    powerbi.visuals.plugins["circleCard6B524826D5D448269A0C4E1DDA156637_DEBUG"] = circleCard6B524826D5D448269A0C4E1DDA156637_DEBUG;
+}
+export default circleCard6B524826D5D448269A0C4E1DDA156637_DEBUG;
